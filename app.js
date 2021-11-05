49fb601f205840097868f3d2b55eee60df2eff99
EMRAH ÖZTÜRK <emrahozturk771@gmail.com>
2021-11-06 00:15:51 +0300

    Static files uploaded

--- app.js
+++ app.js
@@ -1,3 +1,18 @@
-const fn = function () {
-  console.log('function working');
-};
+const express = require('express');
+const path=require('path'); //DEFINE PATH FOR TEMP
+const app = express();
+
+//MIDDLEWARE
+app.use(express.static('public'))
+
+app.get('/', (req, res) => {
+//   const blog = { id: 1, title: 'Blog title', description: 'Blog description' };
+
+  res.sendFile(path.resolve(__dirname,'temp/index.html')); //SEND IN TEMP FILE 
+});
+
+const port = 3000;
+
+app.listen(port, () => {
+  console.log(`SERVER AT ${port} STARTED`);
+});
